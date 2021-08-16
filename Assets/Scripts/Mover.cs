e97cecc88378b924409aa979f677e087c58a1a6a
Michael Watts <wattsjmichael@gmail.com>
2021-08-16 11:28:44 -0700

    Adds walking animation and Knight Prefab

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -12,10 +12,11 @@ public class Mover : MonoBehaviour
  
   void Update()
   {
-    if (Input.GetMouseButtonDown(0))
+    if (Input.GetMouseButton(0))
     {
       MoveToCursor();
     }
+    UpdateAnimator();
   }
 
   private void MoveToCursor()
@@ -30,6 +31,14 @@ public class Mover : MonoBehaviour
         GetComponent<NavMeshAgent>().destination = hit.point;
         Debug.Log(hit.point);
       } 
+  }
+
+  private void UpdateAnimator()
+  {
+      Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+      Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+      float speed = localVelocity.z;
+      GetComponent<Animator>().SetFloat("forwardSpeed", speed);
   }
     }
  
