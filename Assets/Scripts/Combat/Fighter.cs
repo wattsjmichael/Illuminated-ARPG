e7c06706d5d9b828d62cf82aca534f7d204665dc
Michael Watts <wattsjmichael@gmail.com>
2021-08-29 08:09:29 -0700

    Adds Portals, working on adding multiple portals

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -77,6 +77,7 @@ namespace RPG.Combat
     {
       TriggerStopAttack();
       target = null;
+      GetComponent<Mover>().Cancel();
     }
 
     private void TriggerStopAttack()
---
+++ Assets/Scripts/Scene Management/Portal.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.AI;
+
+
+namespace RPG.SceneManagment
+{
+  public class Portal : MonoBehaviour
+  {
+    [SerializeField] int sceneToLoad = -1;
+    [SerializeField] Transform spawnPoint;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+      if (other.CompareTag("Player"))
+      {
+        StartCoroutine(Transition());
+      }
+
+    }
+
+    private IEnumerator Transition()
+    {
+      DontDestroyOnLoad(gameObject);
+      yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
+      Portal otherPortal = GetOtherPortal();
+      UpdatePlayer(otherPortal);
+
+      Destroy(gameObject);
+    }
+
+    private void UpdatePlayer(Portal otherPortal)
+    {
+      GameObject player = GameObject.FindWithTag("Player");
+      player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+      player.transform.rotation = otherPortal.spawnPoint.rotation;
+    }
+
+    private Portal GetOtherPortal()
+    {
+
+      foreach (Portal portal in FindObjectsOfType<Portal>())
+      {
+        if (portal == this) continue;
+        return portal;
+      }
+      return null;
+    }
+  }
+}
