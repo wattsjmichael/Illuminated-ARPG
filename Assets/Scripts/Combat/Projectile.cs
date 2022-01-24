39c63d3cecf781df85976d8cfd9d5801ec8d0739
Michael Watts <wattsjmichael@gmail.com>
2022-01-24 09:31:02 -0800

    Adds Trailing Arrow and Multiple Colors of Enemies

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -6,35 +6,45 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
 
-[SerializeField] float speed = 1;
-
-Health target = null;
-
-
-    // Start is called before the first frame update
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(target == null) return;
-
-        transform.LookAt(GetAimLocation());
-        transform.Translate(Vector3.forward * speed *Time.deltaTime);
-    }
+  [SerializeField] float speed = 1;
+  Health target = null;
+  float damage = 0;
 
 
-public void SetTarget(Health target)
-{
+  // Start is called before the first frame update
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (target == null) return;
+
+    transform.LookAt(GetAimLocation());
+    transform.Translate(Vector3.forward * speed * Time.deltaTime);
+  }
+
+
+  public void SetTarget(Health target, float damage)
+  {
     this.target = target;
+    this.damage = damage;
+  }
+
+  private Vector3 GetAimLocation()
+  {
+    CapsuleCollider targetCapsule = target.GetComponent<CapsuleCollider>();
+    if (targetCapsule == null)
+    {
+      return target.transform.position;
+    }
+    return target.transform.position + Vector3.up * targetCapsule.height / 2;
+  }
+
+
+private void OnTriggerEnter(Collider other)
+{
+   if(other.GetComponent<Health>() != target) return; 
+   target.TakeDamage(damage);
+   Destroy(gameObject);
 }
 
-    private Vector3 GetAimLocation()
-    {
-        CapsuleCollider targetCapsule = target.GetComponent<CapsuleCollider>();
-        if (targetCapsule == null) 
-        {
-            return target.transform.position;
-        }
-        return target.transform.position + Vector3.up * targetCapsule.height / 2;
-    }
 }
